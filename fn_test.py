f1d993dd86dff21d66f8c0dfc42975147cc4146e
Fouda <foudamoustafa@yahoo.com>
2024-02-23 15:34:09 +0300

    optimizing the test

--- fn_test.py
+++ fn_test.py
@@ -1,3 +1,9 @@
+"""
+including unit tests and performance measurement for the Floyd.
+
+Warshall recursive functions.
+"""
+
 import unittest
 import time
 from Floyd_recursive_function import floyd_warshall_recursive, floyd_warshall_recursive_wrapper
@@ -7,11 +13,7 @@ class TestFloydWarshallRecursive(unittest.TestCase):
     """Test case for Floyd-Warshall recursive functions."""
 
     def test_floyd_warshall_recursive_example(self):
-        """
-        Test the example case for the Floyd-Warshall.
-
-        recursive function.
-        """
+        """Test the example case for the Floyd-Warshall recursive function."""
         graph_example = [
             [0, 7, float('inf'), 8],
             [float('inf'), 0, 5, float('inf')],
@@ -35,13 +37,8 @@ class TestFloydWarshallRecursive(unittest.TestCase):
             [float('inf'), float('inf'), float('inf'), 0]
         ]
         result = floyd_warshall_recursive_wrapper(graph_example)
-        # Add your assertions for the wrapper function here
         self.assertIsNotNone(result)
-        # You can add more specific assertions based on the expected behavior
 
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 def measure_performance(graph_function, graph, num_iterations=10):
@@ -50,14 +47,14 @@ def measure_performance(graph_function, graph, num_iterations=10):
 
     Parameters
 ----------
-    - graph_function: The function to measure the performance of.
-    - graph: The input graph for the function.
-    - num_iterations: The number of iterations for performance measurement.
+        graph_function: The function to measure the performance of.
+        graph: The input graph for the function.
+        num_iterations: The number of iterations for performance measurement.
 
     Returns
 -------
-    - Tuple containing the result of the function and the average
-    execution time.
+        Tuple containing the result of the function and the average
+        execution time.
     """
     total_execution_time = 0
     for _ in range(num_iterations):
@@ -66,4 +63,8 @@ def measure_performance(graph_function, graph, num_iterations=10):
         end_time = time.perf_counter()
         total_execution_time += end_time - start_time
     average_execution_time = total_execution_time / num_iterations
-    return result, average_execution_time
+    return result, f"{average_execution_time:.8f}"
+
+
+if __name__ == '__main__':
+    unittest.main()
